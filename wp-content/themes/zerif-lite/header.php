3a4d8e3b8c7276234e3b6238d121395045caeb79
amandatavares <amandatvt@hotmail.com>
2016-04-04 16:22:36 -0300

    Custom more colors and header.

--- wp-content/themes/zerif-lite/header.php
+++ wp-content/themes/zerif-lite/header.php
@@ -105,11 +105,11 @@ wp_head(); ?>
 						
 							if( file_exists(get_stylesheet_directory()."/images/logo.png")):
 							
-								echo '<img src="'.get_stylesheet_directory_uri().'/images/logo.png" alt="'.esc_attr( get_bloginfo('title') ).'">';
+								echo '<img src="'.get_stylesheet_directory_uri().'/images/logo.png" class="logo" alt="'.esc_attr( get_bloginfo('title') ).'">';
 							
 							else:
 								
-								echo '<img src="'.get_template_directory_uri().'/images/logo.png" alt="'.esc_attr( get_bloginfo('title') ).'">';
+								echo '<img src="'.get_template_directory_uri().'/images/logo.png" class="logo" alt="'.esc_attr( get_bloginfo('title') ).'">';
 								
 							endif;
 
